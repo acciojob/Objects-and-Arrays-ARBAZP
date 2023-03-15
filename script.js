36120b5b6bcd1130908d6c0e4ad3432aa28ab445
ARBAZP <117530037+ARBAZP@users.noreply.github.com>
2023-03-15 20:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const person = {
       age: 80
     };
 
+
+// Create a reference to players array using the variable team
+const team = players;
+
+// Create a copy of players array using the spread operator
+const team1 = [...players];
+
+// Create a copy of person object using Object.assign()
+const cap1 = Object.assign({}, person);
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 
